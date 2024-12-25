0d407cc0e7c3b1e4f138412b2cbe4f91bec7992e
gt.park <smileboy0014@gmail.com>
2024-12-25 17:16:59 +0900

    first commit

---
+++ src/main/kotlin/com/kopring/fitlinkbe/FitLinkBeApplication.kt
@@ -0,0 +1,11 @@
+package com.kopring.fitlinkbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitLinkBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<FitLinkBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/kopring/fitlinkbe/FitLinkBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kopring.fitlinkbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitLinkBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
